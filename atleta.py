cf969f6e827f1883b5944030c266484f9d98eb71
dlsantt <dleozin12@gmail.com>
2024-12-18 11:59:49 -0300

    Add files via upload

---
+++ atleta.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+def ler_valor_nao_vazio(nome_variavel):
+    valor_lido = input(f'Entre com o valor para {nome_variavel} do atleta: ')
+    while valor_lido == '':
+        print(f'O valor para {nome_variavel} não pode ser vazio!')
+        valor_lido = input(f'Entre com o valor para {nome_variavel} do atleta: ')
+    return valor_lido
+
+def verifica_data_valida(data_texto):
+    try:
+        data_valida = datetime.strptime(data_texto, "%d/%m/%Y")
+        return True
+    except:
+        return False
+
+def ler_data_valida():
+    dataNascimentoString = input('Entre com a data de nascimento: ')
+    while not verifica_data_valida(dataNascimentoString):
+        print('Data inválida!')
+        dataNascimentoString = input('Entre com a data de nascimento: ')
+    dataNascimento = datetime.strptime(dataNascimentoString, "%d/%m/%Y")
+
+    return dataNascimento
+
+def ler_atleta():
+    nome = ler_valor_nao_vazio('nome')
+    especie = ler_valor_nao_vazio('espécie')
+    dataNascimento = ler_data_valida()
+    tutor = ler_valor_nao_vazio('nome do tutor')
+
+    atleta = {
+        'nome': nome,
+        'especie': especie,
+        'dataNascimento': dataNascimento,
+        'tutor': tutor
+    }
+
+    return atleta
+
+def imprimir_atleta(atleta):
+    print(f"Nome:\t\t{atleta['nome']}")
+    print(f"Espécie:\t{atleta['especie']}")
+    print(f"Data:\t\t{atleta['dataNascimento'].strftime('%d/%m/%Y')}")
+    print(f"Tutor:\t\t{atleta['tutor']}")
+
+meu_bixinho = ler_atleta()
+meu_outro_bixinho = ler_atleta()
+imprimir_atleta(meu_bixinho)
+imprimir_atleta(meu_outro_bixinho)
